ae6ce6cbdd64fb0ef366278c53cc0072fcbf6f0c
Miynevo <95376967+Miynevo@users.noreply.github.com>
2021-12-12 14:13:42 +0200

    New textures
    New Textures

---
+++ My project (1)/Assets/Scripts/Howtoplay.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Howtoplay : MonoBehaviour
+{
+    
+    public void goBack()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+
+}
